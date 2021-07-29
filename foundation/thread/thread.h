9dbcc49f8766a451032ef2b05f65d87016f89aae
caozhiyi <272653256@qq.com>
2021-07-29 19:56:52 +0800

    fmt(thread): format thread code.

--- foundation/thread/thread.h
+++ foundation/thread/thread.h
@@ -2,54 +2,57 @@
 // that can be found in the LICENSE file.
 
 // Author: caozhiyi (caozhiyi5@gmail.com)
+// Copyright <caozhiyi5@gmail.com>
 
-#ifndef COMMON_THREAD_THREAD
-#define COMMON_THREAD_THREAD
+#ifndef FOUNDATION_THREAD_THREAD_H_
+#define FOUNDATION_THREAD_THREAD_H_
 
-#include <thread>     // about thread
-#include <atomic>     // for atomic_bool
-#include <memory>     // for shared_ptr
-#include <functional> // for bind
+#include <thread>   // about thread
+#include <atomic>   // for atomic_bool
+#include <memory>   // for shared_ptr
+#include <functional>  // for bind
 
 namespace fdan {
 
 class Thread {
-public:
-    Thread(): _stop(true) {}
-    virtual ~Thread() {}
+ public:
+  Thread(): _stop(true) {}
+  virtual ~Thread() {}
 
-    //base option
-    virtual void Start() {
-        _stop = false;
-        if (!_thread) {
-            _thread = std::unique_ptr<std::thread>(new std::thread(std::bind(&Thread::Run, this)));
-        }
+  // base option
+  virtual void Start() {
+    _stop = false;
+    if (!_thread) {
+      _thread = std::unique_ptr<std::thread>(
+        new std::thread(std::bind(&Thread::Run, this)));
     }
+  }
 
-    virtual void Stop() {
-        _stop = true;
+  virtual void Stop() {
+    _stop = true;
+  }
+
+  virtual void Join() {
+    if (_thread && _thread->joinable()) {
+      _thread->join();
     }
+  }
+  // TO DO
+  virtual void Run() = 0;
 
-    virtual void Join() {
-        if (_thread && _thread->joinable()) {
-            _thread->join();
-        }
-    }
-    //TO DO
-    virtual void Run() = 0;
+  virtual bool IsStop() {
+    return _stop;
+  }
 
-    virtual bool IsStop() {
-        return _stop;
-    }
+ protected:
+  Thread(const Thread&) = delete;
+  Thread& operator=(const Thread&) = delete;
 
-protected:
-    Thread(const Thread&) = delete;
-    Thread& operator=(const Thread&) = delete;
-
-protected:
-    std::atomic_bool _stop;
-    std::unique_ptr<std::thread> _thread;
+ protected:
+  std::atomic_bool _stop;
+  std::unique_ptr<std::thread> _thread;
 };
 
-}
-#endif
+}  // namespace fdan
+
+#endif  // FOUNDATION_THREAD_THREAD_H_
--- foundation/thread/thread_with_queue.h
+++ foundation/thread/thread_with_queue.h
@@ -2,45 +2,47 @@
 // that can be found in the LICENSE file.
 
 // Author: caozhiyi (caozhiyi5@gmail.com)
+// Copyright <caozhiyi5@gmail.com>
 
-#ifndef COMMON_THREAD_THREAD_WITH_QUEUE
-#define COMMON_THREAD_THREAD_WITH_QUEUE
+#ifndef FOUNDATION_THREAD_THREAD_WITH_QUEUE_H_
+#define FOUNDATION_THREAD_THREAD_WITH_QUEUE_H_
 
-#include "thread.h"
-#include "../structure/thread_safe_block_queue.h"
+#include <utility>
+#include "foundation/thread/thread.h"
+#include "foundation/structure/thread_safe_block_queue.h"
 
 namespace fdan {
-    
+
 template<typename T>
-class ThreadWithQueue: 
-    public Thread {
+class ThreadWithQueue:
+  public Thread {
+ public:
+  ThreadWithQueue() {}
+  virtual ~ThreadWithQueue() {}
 
-public:
-    ThreadWithQueue() {}
-    virtual ~ThreadWithQueue() {}
+  uint32_t GetQueueSize() {
+    return queue_.Size();
+  }
 
-    uint32_t GetQueueSize() {
-        return _queue.Size();
-    }
+  void Push(const T& t) {
+    queue_.Push(t);
+  }
 
-    void Push(const T& t) {
-        _queue.Push(t);
-    }
+  T Pop() {
+    return std::move(queue_.Pop());
+  }
 
-    T Pop() {
-        return std::move(_queue.Pop());
-    }
+  // TO DO
+  virtual void Run() = 0;
 
-    //TO DO
-    virtual void Run() = 0;
+ protected:
+  ThreadWithQueue(const ThreadWithQueue&) = delete;
+  ThreadWithQueue& operator=(const ThreadWithQueue&) = delete;
 
-protected:
-    ThreadWithQueue(const ThreadWithQueue&) = delete;
-    ThreadWithQueue& operator=(const ThreadWithQueue&) = delete;
-
-private:
-    ThreadSafeBlockQueue<T> _queue;
+ private:
+  ThreadSafeBlockQueue<T> queue_;
 };
 
-}
-#endif
+}  // namespace fdan
+
+#endif  // FOUNDATION_THREAD_THREAD_WITH_QUEUE_H_
